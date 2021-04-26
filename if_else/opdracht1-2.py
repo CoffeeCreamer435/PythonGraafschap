46ad46c33c99dde7d767e786826e44abd4830a11
Ido <Ido@DESKTOP-GUGJTDF>
2021-04-26 19:50:15 +0200

    elseif

---
+++ if_else/opdracht1-2.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+print('Welkom bij dit programma')
+print('Wat is jouw leeftijd?')
+age = int(input())
+print('Wat is jouw geslacht? Typ "Man" of "Vrouw"')
+sex = input()
+if sex == 'Man':
+    print('Jij hebt een sluf')
+elif sex == 'Vrouw':
+    print('Er zit een gat in')
+else:
+    print('Sorry geen helicopters toegestaan!')
